a0622145e77a9072c8cc1998fef31e834da326e3
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-10-09 08:13:46 -0300

    draw a line on the screen

--- src/graphics.py
+++ src/graphics.py
@@ -1,6 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
 
-
 class Window:
     def __init__(self, width, height):
         self.width = width
@@ -20,6 +19,27 @@ class Window:
         self.wRunning = True
         while self.wRunning == True:
             self.redraw()
+        print("CLOSED")
     def close(self):
         self.wRunning = False
+
+    def draw_line(self, line: 'Line', fill_color: str):
+        line.draw(self.canvas, fill_color)
+
+class Line:
+    def __init__(self, pointA, pointB) -> None:
+        self.x1 = pointA.x
+        self.y1 = pointA.y
+        self.x2 = pointB.x
+        self.y2 = pointB.y
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
+        )
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-from window import *
+from graphics import *
 
 def main():
     win = Window(800, 600)
+    pntA = Point(700, 100)
+    pntB = Point(300, 300)
+    ln = Line(pntA, pntB)
+    win.draw_line(ln, 'black')
     win.wait_for_close()
 main()
