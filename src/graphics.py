31c2fdbf84abe8931f8af0545fea251a628bf9f1
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-10-11 10:25:41 -0300

    Now we can generate mazes

--- src/graphics.py
+++ src/graphics.py
@@ -1,4 +1,5 @@
 from tkinter import BOTH, Canvas, Tk
+from time import sleep
 
 
 class Window:
@@ -41,12 +42,13 @@ class Point:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+
 class Cell:
-    def __init__(self, top: bool, bottom: bool, left: bool, right: bool, x1: int, y1:int, x2: int, y2:int, win: Window) -> None:
-        self.left = left
-        self.right = right
-        self.top = top
-        self.bottom = bottom
+    def __init__(self, x1: int, y1:int, x2: int, y2:int, win: Window, sides: tuple = (1,1,1,1)) -> None:
+        self.top = sides[0]
+        self.bottom = sides[1]
+        self.left = sides[2]
+        self.right = sides[3]
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -54,23 +56,73 @@ class Cell:
         self.win = win
 
     def draw(self):
-        if self.left:
+        if self.left == 1:
             pntA = Point(self.x1, self.y1)
             pntB = Point(self.x1, self.y2)
             ln = Line(pntA, pntB)
             self.win.draw_line(ln, 'black')
-        if self.right:
+        if self.right == 1:
             pntA = Point(self.x2, self.y1)
             pntB = Point(self.x2, self.y2)
             ln = Line(pntA, pntB)
             self.win.draw_line(ln, 'black')
-        if self.top:
+        if self.top == 1:
             pntA = Point(self.x1, self.y1)
             pntB = Point(self.x2, self.y1)
             ln = Line(pntA, pntB)
             self.win.draw_line(ln, 'black')
-        if self.bottom:
+        if self.bottom == 1:
             pntA = Point(self.x1, self.y2)
             pntB = Point(self.x2, self.y2)
             ln = Line(pntA, pntB)
             self.win.draw_line(ln, 'black')
+
+    def draw_move(self, to_cell: 'Cell', undo=False):
+        clr = 'red'
+        if undo:
+            clr = 'gray'
+        pntA = Point(self.x1+round((self.x2-self.x1)/2), self.y1+round((self.y2-self.y1)/2))
+        pntB = Point(to_cell.x1+round((to_cell.x2-to_cell.x1)/2), to_cell.y1+round((to_cell.y2-to_cell.y1)/2))
+        ln = Line(pntA, pntB)
+        self.win.draw_line(ln, clr)
+
+class Maze:
+    def __init__(self,
+                 x1: int,
+                 y1: int,
+                 num_rows: int,
+                 num_cols: int,
+                 cell_size_x: int,
+                 cell_size_y: int,
+                 win: Window,
+                 ) -> None:
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.__cells: list[list] = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        if self.win == None:
+            return
+        for i in range(self.num_cols):
+            cells_col = []
+            for j in range(self.num_rows):
+                cl = Cell(self.x1+self.cell_size_x*i, self.y1+self.cell_size_y*j, self.x1+self.cell_size_x, self.y1+self.cell_size_y, self.win)
+                cells_col.append(cl)
+            self.__cells.append(cells_col)
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self.__draw_cell(i, j)
+    def __draw_cell(self, i: int, j: int):
+        self.__cells[i][j].draw()
+        self.__animate()
+    def __animate(self):
+        if self.win == None:
+            return
+        self.win.redraw()
+        sleep(0.05)
--- src/main.py
+++ src/main.py
@@ -2,15 +2,7 @@ from graphics import *
 
 def main():
     win = Window(1000, 1000)
-    cl1 = Cell(True, False, False, True, 100, 100, 200, 200, win)
-    cl2 = Cell(True, False, True, False, 200, 100, 300, 200, win)
-    cl3 = Cell(False, True, True, False, 100, 200, 200, 300, win)
-    cl4 = Cell(False, True, False, True, 200, 200, 300, 300, win)
-
-    cl1.draw()
-    cl2.draw()
-    cl3.draw()
-    cl4.draw()
+    Maze(100, 100, 8, 8, 100, 100, win)
 
     win.wait_for_close()
 main()
