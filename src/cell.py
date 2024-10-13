91d34cad39ac5e58bfbd8feff2a566917305376d
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-10-13 02:22:15 -0300

    ft: now have entrance and exit

--- src/cell.py
+++ src/cell.py
@@ -2,7 +2,7 @@ from graphics import Line, Point, Window
 
 
 class Cell:
-    def __init__(self, x1: int, y1:int, x2: int, y2:int, win: Window, sides: tuple = (1,1,1,1)) -> None:
+    def __init__(self, x1: int, y1:int, x2: int, y2:int, win: Window|None = None, sides: tuple = (1,1,1,1)) -> None:
         self.top = sides[0]
         self.bottom = sides[1]
         self.left = sides[2]
@@ -14,28 +14,40 @@ class Cell:
         self.win = win
 
     def draw(self):
-        if self.left == 1:
-            pntA = Point(self.x1, self.y1)
-            pntB = Point(self.x1, self.y2)
-            ln = Line(pntA, pntB)
-            self.win.draw_line(ln, 'black')
-        if self.right == 1:
-            pntA = Point(self.x2, self.y1)
-            pntB = Point(self.x2, self.y2)
-            ln = Line(pntA, pntB)
-            self.win.draw_line(ln, 'black')
+        if self.win == None:
+            return
+
+        top_side = Line(Point(self.x1, self.y1),
+                        Point(self.x2, self.y1))
+        bottom_side = Line(Point(self.x1, self.y2),
+                           Point(self.x2, self.y2))
+        left_side = Line(Point(self.x1, self.y1), 
+                         Point(self.x1, self.y2))
+        right_side = Line(Point(self.x2, self.y1),
+                          Point(self.x2, self.y2))
         if self.top == 1:
-            pntA = Point(self.x1, self.y1)
-            pntB = Point(self.x2, self.y1)
-            ln = Line(pntA, pntB)
-            self.win.draw_line(ln, 'black')
+            self.win.draw_line(top_side, 'black')
+        else:
+            self.win.draw_line(top_side, 'white')
+
         if self.bottom == 1:
-            pntA = Point(self.x1, self.y2)
-            pntB = Point(self.x2, self.y2)
-            ln = Line(pntA, pntB)
-            self.win.draw_line(ln, 'black')
+            self.win.draw_line(bottom_side, 'black')
+        else:
+            self.win.draw_line(bottom_side, 'white')
+
+        if self.left == 1:
+            self.win.draw_line(left_side, 'black')
+        else:
+            self.win.draw_line(left_side, 'white')
+
+        if self.right == 1:
+            self.win.draw_line(right_side, 'black')
+        else:
+            self.win.draw_line(right_side, 'white')
 
     def draw_move(self, to_cell: 'Cell', undo=False):
+        if self.win == None:
+            return
         clr = 'red'
         if undo:
             clr = 'gray'
--- src/maze.py
+++ src/maze.py
@@ -1,6 +1,8 @@
-from graphics import Window
-from cell import Cell
 from time import sleep
+
+from cell import Cell
+from graphics import Window
+
 
 class Maze:
     def __init__(self,
@@ -10,7 +12,7 @@ class Maze:
                  num_cols: int,
                  cell_size_x: int,
                  cell_size_y: int,
-                 win: Window,
+                 win: Window|None=None,
                  ) -> None:
         self.x1 = x1
         self.y1 = y1
@@ -19,31 +21,44 @@ class Maze:
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
-        self.__cells: list[list] = []
+        self._cells: list[list[Cell]] = []
         self.__create_cells()
 
     def __create_cells(self):
         if self.win == None:
             return
 
-        for i in range(self.num_cols+1):
+        for i in range(self.num_cols):
             cells_col = []
-            for j in range(self.num_rows+1):
-                cl = Cell(self.x1+self.cell_size_x*i, self.y1+self.cell_size_y*j, self.x1+self.cell_size_x, self.y1+self.cell_size_y, self.win)
+            for j in range(self.num_rows):
+                cl = Cell(
+                    self.x1+self.cell_size_x*i, 
+                    self.y1+self.cell_size_y*j, 
+                    self.x1+self.cell_size_x*(i+1), 
+                    self.y1+self.cell_size_y*(j+1), 
+                    self.win)
                 cells_col.append(cl)
-            self.__cells.append(cells_col)
-
-        for i in range(self.num_cols+1):
-            for j in range(self.num_rows+1):
+            self._cells.append(cells_col)
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
                 self.__draw_cell(i, j)
+        self.__break_entrance_and_exit()
 
     def __draw_cell(self, i: int, j: int):
-        self.__cells[i][j].draw()
+        self._cells[i][j].draw()
         self.__animate()
 
     def __animate(self):
         if self.win == None:
             return
-
         self.win.redraw()
         sleep(0.05)
+
+    def __break_entrance_and_exit(self):
+        if self.win == None or self._cells == []:
+            return
+
+        self._cells[0][0].top = 0
+        self.__draw_cell(0,0)
+        self._cells[-1][-1].bottom = 0
+        self.__draw_cell(-1,-1)
